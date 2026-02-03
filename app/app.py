a70511dd50951e1782249d236a96a51a61c96b58
madebyemmanuel-r <emmanuelrabiu89@gmail.com>
2026-02-03 19:11:31 +0000

    Fixing navigation (by removing  the broken forgot password route)

--- app/app.py
+++ app/app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 @app.route('/features')
 def features():
