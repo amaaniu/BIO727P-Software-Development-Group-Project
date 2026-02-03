66bf24fa4f619a01e6d45596d7ddf6510871991e
madebyemmanuel-r <emmanuelrabiu89@gmail.com>
2026-02-03 18:43:16 +0000

    Trying to implement home route and update navigation links

--- app/app.py
+++ app/app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
 @app.route('/features')
 def features():
-    return render_template('features.html')
+    return 'soon rendering template features'
+
+@app.route('/documentation')
+def documentation():
+    return 'soon rendering template documentation'
+
+@app.route('/casestudy_tutorial')
+def casestudy_tutorial():
+    return 'soon rendering template casestudy tutorial'
 
 if __name__ == '__main__':
     app.run(debug=True)
