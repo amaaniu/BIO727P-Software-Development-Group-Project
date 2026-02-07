67826be21f57ee1c8d30ec1dad099260dcb221cd
madebyemmanuel-r <emmanuelrabiu89@gmail.com>
2026-02-07 16:05:48 +0000

    Update authentication routes and templates; add registration and logout functionality, update navigation links across templates

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 # This file defines the routes for the authentication blueprint of the Flask application. It includes routes for user login, registration, and logout, as well as any other functionality users must login to access. The routes will render the appropriate templates and handle form submissions for user authentication processes.
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, session, url_for, request, flash
 auth_bp = Blueprint('auth', __name__)
 
 # Creates the route for the login page
@@ -23,3 +23,35 @@ def login():
             flash('Invalid email or password. Please try again.', 'danger') #Uses Flask's flash function to display an error message to the user in bootstrap's alert format
     return render_template('login.html')
 
+
+# Creates the route for the registration page
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    """Renders the registration page and handles user registration."""
+
+    if request.method == 'POST':
+        # Handles the registration form submission by retrieving the email, password, and confirm password from the form data
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        # Check if username or email already exists in the database
+        if user_exists(username, email):
+            flash('Username or email already exists. Please try again.', 'danger')
+        elif password == confirm_password:
+            # Redirect to the dashboard page after successful registration (the user is automatically logged in after registration)
+            return redirect(url_for('main.dashboard'))
+        else:
+            # If the passwords do not match, an error message appears and the user is prompted to try again
+            flash('Passwords do not match. Please try again.', 'danger')
+    return render_template('register.html')
+
+# Creates the route for the logout functionality
+@auth_bp.route('/logout')
+def logout():
+    """Handles user logout."""
+    
+    session.clear()
+    # After logging out, you would redirect them to the home page or login page
+    return redirect(url_for('main.home'))
