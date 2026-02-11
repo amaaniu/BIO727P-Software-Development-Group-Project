31ca2d8d405244cdafd4e22dc64baa49a690f968
madebyemmanuel-r <emmanuelrabiu89@gmail.com>
2026-02-11 12:24:58 +0000

    Refactoring the app

--- app/auth/__init__.py
+++
@@ -1,2 +0,0 @@
-# Sets up the authentication blueprint for the application so that it can be registered in the main app.
-from .routes import auth_bp
--- app/main/__init__.py
+++
@@ -1,5 +0,0 @@
-# Sets up the main blueprint for the application so that it can be registered in the main app.
-from flask import Blueprint
-from .routes import main_bp
-
-
