961e4940702d70f0705db3fb7e30032641bab8d0
madebyemmanuel-r <emmanuelrabiu89@gmail.com>
2026-02-16 13:50:02 +0000

    Another change to Upload bp

--- app/__init__.py
+++ app/__init__.py
@@ -30,11 +30,11 @@ def create_app():
     
     from .main import main_bp 
     from .auth import auth_bp
-    from .uploads import uploads_bp 
+    from .upload import upload_bp 
 
     app.register_blueprint(main_bp)  # Register the main blueprint with a URL prefix for main routes
     app.register_blueprint(auth_bp, url_prefix='/auth')  # Register the auth blueprint with a URL prefix for authentication routes
-    app.register_blueprint(uploads_bp, url_prefix='/uploads')  # Register the uploads blueprint with a URL prefix for upload routes
+    app.register_blueprint(upload_bp, url_prefix='/uploads')  # Register the uploads blueprint with a URL prefix for upload routes
     #app.register_blueprint(other_blueprint)  # Register other blueprints as needed
     
     with app.app_context():
